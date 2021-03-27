3138003592f3dd2d2e8fc3685d25c558929854fd
Raswanth <111801056@smail.iitpkd.ac.in>
2021-03-27 12:48:14 +0530

    NurseDiagnostics view added

---
+++ view_create..sql
@@ -0,0 +1,41 @@
+create view DoctorInfo as (
+    select doctorID, name, phone, email, address, sex from Doctor natural join Employee
+);
+
+create view NurseInfo as (
+    select nurseId, name, phone, email, address, sex from Nurse natural join Employee
+);
+
+create view DriverInfo as (
+    select driverID, name, phone, email, address, sex from Driver natural join Employee
+);
+
+create view AdminInfo as (
+    select adminID, name, phone, email, address, sex from Admin natural join Employee
+);
+
+create view VendorDrugInfo as (
+    select Vendor.vendorID, p.pharmacyID, Vendor.name, Vendor.phone, Vendor.address, Vendor.email,
+         price, orderTime, supplyTime,
+         p.name as drug_name, category
+    from Vendor
+           inner join Supplies s using (vendorID)
+           inner join Pharmacy p using (pharmacyID)
+);
+
+create view DriverJourneyInfo as (
+    select
+           DriverInfo.*, Ambulance.*, Journey.startTime, Journey.endTime, Journey.address as journeyAddress
+    from DriverInfo
+        inner join Journey using (driverID)
+        inner join Ambulance using (ambulanceId)
+)
+
+create view NurseDiagnosticsInfo as (
+	select NurseInfo.ID as Nurse_ID, NurseInfo.qualification, 
+               NurseInfo.license, Diagnostics.ID as Diagnostics_ID, 
+               Diagnostics.category, Diagnostics.name as diagnostics 
+        from NurseInfo inner join (Assists, Diagnostics) on 
+             (NurseInfo.ID = Assists.nurseId and Diagnostics.ID = Assists.diagnosticsID)
+
+);
