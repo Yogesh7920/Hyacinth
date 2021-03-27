a48a1dbe8183a1efadb03536ea854cfdfbef3241
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2021-03-27 12:02:10 +0530

    bugs fixed

--- view_create.sql
+++ view_create.sql
@@ -15,7 +15,12 @@ create view AdminInfo as (
 );
 
 create view VendorDrugInfo as (
-    select * from vendor
-    inner join supplies s on vendor.ID = s.vendorID
-    inner join pharmacy p on s.drugID = p.ID
+    select vendorID, drugID, vendor.name, vendor.phone, vendor.address, vendor.email,
+         price, orderTime, supplyTime,
+         p.name as drug_name, category
+    from vendor
+           inner join supplies s on vendor.ID = s.vendorID
+           inner join pharmacy p on s.drugID = p.ID
 );
+
+
