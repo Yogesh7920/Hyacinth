257f1a67907a05da533c53373307ad8906e7252a
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2021-04-22 17:58:13 +0530

    added function to get the total monthly income of the hospital.

--- sql_scripts/functions.sql
+++ sql_scripts/functions.sql
@@ -1,7 +1,19 @@
 use hyacinth;
 
+drop function if exists total_monthly_income;
+
 DELIMITER //
 
-
+create function total_monthly_income()
+returns float
+begin
+    return (
+        select sum(amount) from hyacinth.invoice
+        inner join appointment a on invoice.invoiceID = a.invoiceId
+        where (isPaid=1) and (startTime > date_sub(curdate(), interval 31 day ))
+);
+end //
 
 DELIMITER ;
+
+select total_monthly_income();
