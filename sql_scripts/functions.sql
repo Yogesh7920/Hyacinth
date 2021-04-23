72ad3d4b088902dd9c4f9713b0f1c6ce80cf382a
subhash011 <subhash011011@gmail.com>
2021-04-23 11:08:10 +0530

    added user exists function and insert query needs to be completed in patient reg

--- sql_scripts/functions.sql
+++ sql_scripts/functions.sql
@@ -1,7 +1,6 @@
-use hyacinth;
-
 drop function if exists total_monthly_income;
 drop function if exists total_monthly_expenditure;
+drop function if exists user_exists;
 
 DELIMITER //
 
@@ -9,8 +8,8 @@ create function total_monthly_income()
 returns float
 begin
     return (
-        select sum(amount) from hyacinth.invoice
-        inner join appointment a on invoice.invoiceID = a.invoiceId
+        select sum(amount) from Hyacinth.Invoice
+        inner join Appointment a on Invoice.invoiceID = a.invoiceId
         where (isPaid=1) and (startTime > date_sub(curdate(), interval 31 day ))
 );
 end //
@@ -19,8 +18,16 @@ create function total_monthly_expenditure()
     returns float
 begin
     return (
-        select sum(price) from hyacinth.supplies
+        select sum(price) from Hyacinth.Supplies
         where (orderTime > date_sub(curdate(), interval 31 day ))
+    );
+end //
+
+create function user_exists(email varchar(20))
+    returns bool
+begin
+    return (
+        exists(select User from mysql.user where User=email)
     );
 end //
 
--- sql_scripts/procedure.sql
+++ sql_scripts/procedure.sql
@@ -1,25 +1,56 @@
-use hyacinth;
+drop procedure if exists exec_query;
+drop procedure if exists EmployeeRegistration;
+drop procedure if exists PatientRegistration;
 
 delimiter //
 
-create procedure Register(in email_ varchar(11), in password_ varchar(256), out valid bool)
+create procedure exec_query(query_str varchar(255))
 begin
-    if exists(
-        select User
-        from mysql.user
-        where User=email_
-        ) then
-            set @password = (select password from employee where email=email_);
-            if (password(password_)=@password) then
-                set valid=true;
-            else
-                set valid=false;
-            end if;
-    else
-        set @sql = (select concat('create user ', email_, ' identified by ', password_));
-        prepare stmt from @sql;
-        execute stmt;
+    set @exec_cmd = query_str;
+    prepare stmt from @exec_cmd;
+    execute stmt;
+    deallocate prepare stmt;
+end; //
+
+create procedure EmployeeRegistration(in email_ varchar(20), in password_ varchar(255), out valid bool)
+begin
+    -- If user exists, then check password
+    if user_exists(email_)
+    then
+        set @password = (select password from Employee where email=email_);
+        if (password(password_)=@password) then
+            set valid=true;
+        else
+            set valid=false;
+        end if;
+    -- If user does not exist, check if the admin has added him/her as an employee.
+    elseif exists(select * from Employee where email=email_)
+    then
+        set @create_user = concat('create user ', substring_index(email_, '@', 1), '@localhost identified by \'', password_, '\';');
+        call exec_query(@create_user);
         set valid=true;
+    -- If the admin has not added the Employee, don't allow registration.
+    else
+        set valid=false;
+    end if;
+end; //
+
+create procedure PatientRegistration(name_ varchar(45), password_ varchar(255), phone varchar(20), 
+                                    email varchar(20), address_ varchar(20), sex varchar(20),
+                                    medicalHistory varchar(300), marital bool, out registered bool
+                                    )
+begin
+    if user_exists(email)
+    then
+        set registered=false;
+    else
+        set @create_user = concat('create user ', substring_index(email, '@', 1), '@localhost identified by ', PASSWORD(password_));
+        call exec_query(@create_user);
+
+        -- select @insert_user = concat('insert into Patient ')
+        -- call exec_query(@insert_user);
+        -- grant patient_role to email;
+        set registered=true;
     end if;
 end; //
 
