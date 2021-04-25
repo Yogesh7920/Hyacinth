eb0fa374025f2d61dfcf7625cb7b26e4874d2caf
Yogesh7920 <111801047@smail.iitpkd.ac.in>
2021-04-25 12:41:16 +0530

    refactored backend code

--- hyacinthBE/routers/nurse.py
+++ hyacinthBE/routers/nurse.py
@@ -1,8 +1,6 @@
-import os
-import sys
-
 from fastapi import APIRouter
-import mariadb
+from database import Global
+
 
 router = APIRouter(
     prefix="/nurse",
@@ -10,24 +8,7 @@ router = APIRouter(
     responses={404: {"description": "Not found"}}
 )
 
-try:
-    user = os.getenv("DB_USER", "root")
-    password = os.getenv("DB_PASSWORD", "password")
-    host = os.getenv("DB_HOST", "localhost")
-    port = int(os.getenv("DB_PORT", "3306"))
-    database = os.getenv("DB", "Hyacinth")
-    conn = mariadb.connect(
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-        database=database
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-cur = conn.cursor()
+cur = Global.cur
 
 
 @router.get('/')
